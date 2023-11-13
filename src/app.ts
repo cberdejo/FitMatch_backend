00cb75149dfb7afed95d18d486d4b30655245de2
Chr1s2205 <63378128+Chr1s2205@users.noreply.github.com>
2023-11-13 17:42:08 +0100

    expressJson fix

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import cors from 'cors';
+import usuarioRouter from './routes/usuario_routes';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.use('/', usuarioRouter);
+
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en http://localhost:${port} `);
+});
+
